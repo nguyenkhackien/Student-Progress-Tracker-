2ddcea70e8fda6dcb32a74e8d6d3f04fd6a17028
nguyenkhackien <20020679@vnu.edu.vn>
2024-09-20 22:01:56 +0700

    test commit

--- BackEnd/src/api/routes/route.js
+++ BackEnd/src/api/routes/route.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const { createData } = require("../controllers/CreateData")
 
 router.get("/createData", createData)
-router.get("/", (req, res) => res.send("hello"))
+router.get("/", (req, res) => res.send("hello khac kien !"))
 module.exports = router
